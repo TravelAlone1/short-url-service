1630a766fdce55bd036c7ee9f734559dbaab4e76
lx <2734572804@qq.com>
2019-09-26 16:00:44 +0800

    记录调用接口

---
+++ src/main/java/com/lx/shorturl/annotation/RequestLimit.java
@@ -0,0 +1,8 @@
+package com.lx.shorturl.annotation;
+
+/**
+ * @Author: lx
+ * @Date: 2019/9/26 15:46
+ */
+public class RequestLimit {
+}
---
+++ src/main/java/com/lx/shorturl/aspect/RequestLimitAspect.java
@@ -0,0 +1,8 @@
+package com.lx.shorturl.aspect;
+
+/**
+ * @Author: lx
+ * @Date: 2019/9/26 15:55
+ */
+public class RequestLimitAspect {
+}
